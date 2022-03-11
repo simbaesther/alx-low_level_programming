d58de1b52fdf3229ecd8649e606f37be3bb4e8d1
simbaesther <simbaesther@gmail.com>
2022-03-11 04:54:25 -0800

    a program that prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - a program that prints the alphabet in lowercase, 
+ * main - a program that prints the alphabet in lowercase,
  * followed by a new line.
  * Return: 0 on Success
  */
