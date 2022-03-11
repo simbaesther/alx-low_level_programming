dc4b2a002f496856f96a20a75a0274f606472fdb
simbaesther <simbaesther@gmail.com>
2022-03-11 05:48:42 -0800

    Prints the alphabet except the letters q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main : prints the alphabet in lowercase,
+ * except q and e
+ * Return: 0 on Success
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
