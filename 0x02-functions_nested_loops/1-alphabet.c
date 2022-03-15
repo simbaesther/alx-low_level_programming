9378473d52c408a0fb3199c20f04b87d82d35926
simbaesther <simbaesther@gmail.com>
2022-03-15 08:34:34 -0700

    prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,9 @@
-#include "main.h"
 /**
  * Desc: file 1-alphabet.c
- * Prototype: void print_alphabet(void);
+ * Prototype: void print_alphabet(void)
+ */
+#include "main.h"
+/**
  * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
  * using _putchar
  */
