7590951977bda8d2b4fa5a8e1b35bbaaf594e2fd
simbaesther <simbaesther@gmail.com>
2022-03-15 06:20:45 -0700

    main header

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,7 +5,7 @@
  * Desc: Header file containing declarations for all functions
  * used in the 0x02-functions_nested_loops directory.
  */
-int _putchar(cha c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
