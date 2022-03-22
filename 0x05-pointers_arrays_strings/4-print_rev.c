46dca071e77dfaab93219d3255e95201d4474a09
simbaesther <simbaesther@gmail.com>
2022-03-22 03:48:49 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,14 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int len = 0;
 
-	while (s[index++])
+	while (s[len])
 		len++;
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
-
+	while (len--)
+	{
+		_putchar(s[len]);
+	}
 	_putchar('\n');
 }
