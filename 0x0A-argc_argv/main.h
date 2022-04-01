38f3782d66312aab34590a4762ab3a89beca92c0
Esther Simba <simbaesther@gmail.com>
2022-04-01 17:29:36 +0300

    a program that prints all arguments it receives.

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -9,5 +9,6 @@
 int _putchar(char c);
 int main(int __attribute__((__unused__)) argc, char *argv[]);
 int main(int argc, char __attribute__((__unused__)) *argv[]);
+int main(int argc, char *argv[]);
 
 #endif
