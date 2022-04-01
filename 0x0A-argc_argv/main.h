adb0a7fe2607b722e771fd1ac0eee636c281ecc5
Esther Simba <simbaesther@gmail.com>
2022-04-01 17:52:33 +0300

    a program that adds positive numbers.

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -11,5 +11,6 @@ int main(int __attribute__((__unused__)) argc, char *argv[]);
 int main(int argc, char __attribute__((__unused__)) *argv[]);
 int main(int argc, char *argv[]);
 int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
 
 #endif
