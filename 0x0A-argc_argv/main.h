3062bec420e07208fefc3b1f3fc5c6866d1402d2
Esther Simba <simbaesther@gmail.com>
2022-04-01 17:48:25 +0300

    int main(int argc, char *argv[])

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -10,5 +10,6 @@ int _putchar(char c);
 int main(int __attribute__((__unused__)) argc, char *argv[]);
 int main(int argc, char __attribute__((__unused__)) *argv[]);
 int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
 
 #endif
