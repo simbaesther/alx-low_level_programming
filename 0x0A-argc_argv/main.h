8031b69b077fc287db524857363199d2236fc8c5
Esther Simba <simbaesther@gmail.com>
2022-04-01 16:52:35 +0300

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x0A. C - argc, argv Directory
+ */
+
+int _putchar(char c);
+
+#endif
