efa13567176dc08a0ea68e6c60f70244c6aa6ff6
Esther Simba <simbaesther@gmail.com>
2022-04-05 17:48:11 +0300

    0x0B-malloc_free project

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Desc: Header file containing the prototypes of all
+ * functions and the prototype of the function @_putchar
+ */
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+#endif
