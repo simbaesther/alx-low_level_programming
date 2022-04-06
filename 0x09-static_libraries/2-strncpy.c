14abc1dc5d992ab948286a55ea195fdac3742477
Esther Simba <simbaesther@gmail.com>
2022-04-06 21:55:58 +0300

    changes to static libraries

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies at most an inputted number
+ * of bytes from string src into dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copied from src.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0; /* j represents the length of src */
+
+	while (src[i++])
+		j++;
+
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
+
+	for (i = j; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
