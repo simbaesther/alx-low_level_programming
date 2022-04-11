c8ec311f4224ed957c98f12b62cee774a66145c6
Esther Simba <simbaesther@gmail.com>
2022-04-11 23:07:14 +0300

    resolving errors in 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,11 @@
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * print_dog - a function that prints a struct dog
+ * @d: The struct dog to be printed
+ */
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
