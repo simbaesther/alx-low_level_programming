fcc4a90ef58fe34488c9bce04fd93d221dd5a964
Esther Simba <simbaesther@gmail.com>
2022-04-21 23:08:38 +0300

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,9 @@
-global    _start
-
-          section   .text
-_start:   mov       rax, 1
-	  mov       rdi, 1
-          mov       rsi, message
-          mov       rdx, 13
-          syscall          
-	  mov       rax, 60
-          xor       rdi, rdi
-          syscall
-
-          section   .data
-message:  db        "Hello, Holberton", 10
+global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
