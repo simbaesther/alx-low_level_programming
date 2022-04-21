af803543bf028e6ba059f0c3ee97bcb3c981000d
Esther Simba <simbaesther@gmail.com>
2022-04-22 00:10:21 +0300

    changes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,9 @@
 #include "lists.h"
-
 /**
  * print_list - prints all the elements of a list_t list
  * If str is NULL, print [0] (nil)
  * you are allowed to use printf
  * @h: singly linked list
- *
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
