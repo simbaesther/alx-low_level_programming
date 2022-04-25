3cdf548252f8b1d6987ea91cca2e1150c039dcca
Esther Simba <simbaesther@gmail.com>
2022-04-25 23:55:34 +0300

    adding specified data structure to lists.h file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -6,6 +6,20 @@
 #include <string.h>
 
 /**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+/**
  * Desc: lists.h file contains the prototypes of the
  * functions used in 0x13. C - More singly linked lists
  * and the prototype of the function _putchar
